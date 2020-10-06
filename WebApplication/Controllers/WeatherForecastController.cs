de483a36ca97ff7f9db8695fb7329dfc14a5d1ca
Trishasaballa <trish.saballa@gmail.com>
2020-10-06 11:01:20 +0000

    deserialized activity

--- WebApplication/Controllers/WeatherForecastController.cs
+++ WebApplication/Controllers/WeatherForecastController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Newtonsoft.Json;
 using Serilog;
 
 namespace WebApplication.Controllers
@@ -13,7 +17,7 @@ namespace WebApplication.Controllers
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-          
+
         };
 
         private readonly ILogger _logger;
@@ -24,19 +28,43 @@ namespace WebApplication.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public WeatherForecast Get()
         {
-            _logger.Information("hello {summaries}",Summaries);
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
 
+            WeatherForecast wf = new WeatherForecast();
+            var ng = new Random();
+            string weather = "{\"Date\":\"2020-10-11T03:24:09.4308469+00:00\",\"TemperatureC\":31,\"TemperatureF\":0,\"Summary\":\"Mild\"}";
+            //wf.Date = DateTime.Now.AddDays(0);
+            //wf.TemperatureC = ng.Next(-20, 55);
+            //wf.TemperatureF = 32;
+            //wf.Summary = Summaries[ng.Next(Summaries.Length)];
             
+
+
+            //string output = JsonConvert.SerializeObject(wf);
+            WeatherForecast deserializedWeatherForecast = JsonConvert.DeserializeObject<WeatherForecast>(weather);
+            //deserializedWeatherForecast.TemperatureC = 45;
+
+            //_logger.Information("hello {summaries}", Summaries);
+            //var output1 = JsonConvert.DeserializeObject(output);
+            _logger.Information(string.Format("Date: {0}, TemperatureC: {1}, TemperatureF: {2}, Summary: {3}", deserializedWeatherForecast.Date.ToString(), deserializedWeatherForecast.TemperatureC.ToString(), deserializedWeatherForecast.TemperatureF.ToString(), deserializedWeatherForecast.Summary.ToString()));
+           
+            
+            return deserializedWeatherForecast;
+          
+            
+
         }
+
+        
     }
+
 }
+
+
+
+        
+
+       
+    
+
